e3a6c2ef2c55965cb799c6dde1bee9879e62cd11
Taha daboussi <Taha_daboussi@hotmail.com>
2024-10-17 18:02:05 +0300

    imporving the request linker and finder

--- src/RequestParser/LinkedRequestFinder.ts
+++ src/RequestParser/LinkedRequestFinder.ts
@@ -1,4 +1,4 @@
-import { Helpers } from "src/helpers/Helpers";
+import { Helpers } from "../../src/helpers/Helpers";
 import { Main } from "src/Main";
 const keysToSkip = ['username', 'password', 'email', 'user', 'pass', 'name', 'screen_name', "next_link", "LoginTwoFactorAuthChallenge"]
 
@@ -8,11 +8,21 @@ export class LinkedRequestFinder {
     constructor(Main: Main) {
         this.Main = Main
     }
-    run(initialPair: IRequestResponseArray, data: Array<IRequestResponseArray>) {
+    /**
+     * 
+     * @param initialPair 
+     * @param data 
+     */
+    run(initialPair: IRequestResponseArray, data: Array<IRequestResponseArray>):IMappedRelatedRequests[] {
         this.data = data
-        this.findRelatedRequests(initialPair)
+        return this.findRelatedRequests(initialPair)
     }
-    findRelatedRequests = (initialPair: IRequestResponseArray) => {
+    /**
+     * will map through intercepted requests and find related requests to the initial request 
+     * @param initialPair initial pair to start maping from 
+     * @returns {void}
+     */
+    findRelatedRequests = (initialPair: IRequestResponseArray, mappedRelatedRequests: IMappedRelatedRequests[] = []): IMappedRelatedRequests[] => {
         const requestData = initialPair.requestData;
         if (initialPair && initialPair.requestId !== null) {
             let requestDataPostData: Record<string, string> = {};
@@ -27,15 +37,21 @@ export class LinkedRequestFinder {
                     requestDataPostData = JSON.parse(requestData.postData as string) as Record<string, string>;
                 } catch (e: unknown) {
                     this.Main.log.error('Invalid JSON format in postData: ' + e, requestData.postData);
-                    return;
+                    return mappedRelatedRequests;
                 }
             }
-            const dynamicDataToLookFor = this.scrapeBasedOnQuotations(requestDataPostData);
+            const dynamicDataToLookFor = this.scrapePostData(requestDataPostData);
             const foundResponses = [] as IRequestResponseArray[];
             dynamicDataToLookFor.forEach((key) => {
                 const foundData = this.data.filter(res => JSON.stringify(res.responseData).includes(key))[0];
                 if (foundData && foundData.requestId && (foundData.requestId != initialPair.requestId) && key.length > 3) {
                     this.Main.log.info("Request ID Request : " + initialPair.requestId + " Response Comes From Request ID  : " + foundData.requestId + " Resposne " + " For Value: " + Helpers.GeneralHelpers.shortenKeyIfTooLong(key));
+                    mappedRelatedRequests.push({
+                        requestId: initialPair.requestId,
+                        relatedRequestId: foundData.requestId,
+                        key: key,
+                        requestIndex : initialPair.requestIndex
+                    });
                     foundResponses.push(foundData);
                 } else {
                     if (key.length > 3) {
@@ -49,17 +65,22 @@ export class LinkedRequestFinder {
             });
             if (foundResponses.length > 0) {
                 foundResponses.forEach(response => {
-                    this.findRelatedRequests(response); // Recursively handle each found response
+                    this.findRelatedRequests(response, mappedRelatedRequests); // Recursively handle each found response
                 });
             } else {
                 this.Main.log.warn('Not all related requests were found for Request ID ' + initialPair.requestId);
             }
         }
+        return mappedRelatedRequests;
     };
-
- 
-
-    scrapeBasedOnQuotations = (dataObject: Record<string, string>, dynamicDataToScrape: string[] = []) => {
+    
+    /**
+     * @description will scrape dynamic Data to scrape and look for from requests
+     * @param dataObject data to Look From  
+     * @param dynamicDataToScrape stored dynamic data To scrape
+     * @returns dyname data to scrape
+     */
+    scrapePostData = (dataObject: Record<string, string>, dynamicDataToScrape: string[] = []) => {
         for (const key in dataObject) {
             if (Object.prototype.hasOwnProperty.call(dataObject, key)) {
                 const value = dataObject[key]; // Access the value for each key
@@ -71,7 +92,7 @@ export class LinkedRequestFinder {
                         if (typeof item === 'object' && item !== null) {
                             Object.keys(item).forEach((subKey) => {
                                 if (typeof item[subKey] === 'object') {
-                                    this.scrapeBasedOnQuotations(item[subKey], dynamicDataToScrape); // Recursively pass dynamicDataToScrape
+                                    this.scrapePostData(item[subKey], dynamicDataToScrape); // Recursively pass dynamicDataToScrape
                                 }
                             });
                         }
@@ -80,7 +101,7 @@ export class LinkedRequestFinder {
                 // Check if the value is an object
                 else if (typeof value === 'object' && value !== null) {
                     Object.keys(value).forEach((subKey) => {
-                        this.scrapeBasedOnQuotations(value[subKey], dynamicDataToScrape); // Recursively pass dynamicDataToScrape
+                        this.scrapePostData(value[subKey], dynamicDataToScrape); // Recursively pass dynamicDataToScrape
                     });
                 }
                 // Otherwise, it is a primitive (string, number, etc.)
